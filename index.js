384cc30f6e850c3d9cbe832d9905e427ed43f6bc
WebS2B <ducvuk6196@gmail.com>
2018-07-13 01:22:29 +0700

    update

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
 const express = require('express')
 const jsonfile = require('jsonfile');
+const request = require('request');
 const app = express()
 
 const FILE_OUTPUT = __dirname + '/result.json';
+const access_token = 'EAAXvL47ihEIBAGhsj8ZCNI6JRqjP2dPxn6ZC3XCn1MupYQ1VvtNdHPtAOGJBSqlvEa1lI0UsHBR6SP5bAMU9wxcOrH2b0dIyozH1hkToYJld4i0AEGz5B6eosbkLv7n8e9r2FekVykFZABPIUWZA4FzZBY5ecHR8AzKrcIjPygAZByDNbNggqds98qjZBBSztxxpfHKfbEttwZDZD';
 
 var port = process.env.PORT || 8080;
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
+
+    console.log(req.path);
     res.render('index');
 });
 
 app.get('/result', function(req, res) {
+
+    console.log(req.path);
+    const url = 'https://graph.facebook.com/v2.8/1597163410610277/feed?access_token=' + access_token + '&format=json';
     let arr = [];
-    res.send(arr);
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let t = JSON.parse(body);
+            for (let i = 0; i < t['data'].length; i++) {
+                arr.push(t['data'][i])
+            }
+        }
+    });
+    setTimeout((function() { res.send(arr) }), 1000);
 })
 
-app.get('/articles', function(req, res) {
-    let arr = {};
-    res.render('article', arr);
+app.get('/post/:id', function(req, res) {
+    res.render('article');
 })
 
-app.get('/articles/:id', function(req, res) {
-    if (req.params.slug && req.params.id) {
-        slug = req.params.slug;
-        id = req.params.id;
-    }
-    let dat = [];
-    res.send(dat);
+app.post('/post/:id', function(req, res) {
+    console.log(req.path);
+    const url = 'https://graph.facebook.com/v2.8/' + req.params.id + '/comments?access_token=' + access_token + '';
+    let arr = [];
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let t = JSON.parse(body);
+            for (let i = 0; i < t['data'].length; i++) {
+                arr.push(t['data'][i])
+            }
+        }
+    });
+    setTimeout((function() { res.send(arr) }), 1000);
 })
 
 app.listen(port, () => console.log('Server app listening on port:' + port));
